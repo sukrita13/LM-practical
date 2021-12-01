00581c06754df598d7fa37c608ab36ed5d0ab1c5
sukrita13 <sukritac@gmail.com>
2021-12-01 21:02:13 +0000

    version 1.0

--- Practical Version 1.R
+++ Practical Version 1.R
@@ -1,21 +1,35 @@
+
+#Load libraries
 library(tidyverse)
 library(ggplot2)
 library(magrittr)
 library(corrgram)
 library(ggpubr)
 
+#Data load and clean up 
 pub_data <- read.csv("pub.csv")
-attach(pub_data)
-str(pub_data)
+
 pub_data[,2] <- as.factor(pub_data[,2])
 pub_data[,3] <- as.factor(pub_data[,3])
+colnames(pub_data) <- c("Articles","Female","Marital_Status","No_of_kids","Prestige_Score","Mentor_publications")
 str(pub_data)
-summary(pub_data)
-tab1 <- table(articles, female)
-barplot(tab1, beside = TRUE)
-boxplot(mwt ~ low, xlab= "low bw", ylab="mother weight")
-plot(pub_data, upper_panel = NULL)
+attach(pub_data)
 
+
+#Numerical Summary
+pub_data_summary <- pub_data 
+pub_data_summary[,4] <- as.factor(pub_data_summary[,4])
+summary(pub_data_summary)
+
+
+#Overdispersion summary
+Article_summary <- data.frame(Mean = mean(pub_data_summary[,1]) ,
+                                    Variance = var(pub_data_summary[,1]),
+                                    Ratio = Article_count_summary[,2]/Article_count_summary[,1] )
+
+Article_summary
+
+#Correlation Statistics
 pub_data %>% 
   mutate_if(is.factor, as.numeric) %>% 
   cor %>%
@@ -28,49 +42,56 @@ pub_data %>%
            col.regions = colorRampPalette(c("#FFFFFF","#eff3ff","#c6dbef","#3182bd","#08519c"))
   )
 
-ggplot(pub_data,aes (x=mentor, y = articles))+
-  geom_point(position = position_jitter(width = 0.1))+
-  theme(legend.position = "top",
-        legend.direction = "horizontal",
-        plot.title = element_text(hjust = 0.5),
-        axis.text.x = element_text(angle = 45, vjust = 1, hjust = 1))+
-  labs(title = "Data distribution by category levels", x = "Mentor publications", y = "Articles published" )+
-  scale_x_discrete()+
-  facet_grid(~female)
+#Logic for removing marital status
+pub_data %>%
+  group_by (Marital_Status, No_of_kids) %>% 
+  summarize(value = length(Marital_Status))%>%
+  ggplot(aes(fill= Marital_Status, y= value, x= No_of_kids)) + 
+  geom_bar(position = position_dodge2(preserve = "single",padding = 0) , 
+           stat="identity",
+           colour = "black", 
+           width = 0.75)+ 
+  scale_fill_manual(values=c("#eff3ff","#9ecae1"))+
+  labs(title = "Marriage vs kids",  y = "Frequency" )+
+  theme(plot.title = element_text(hjust = 0.5))
 
+#Articles vs poisson distribution
+x <- seq(min(Articles), max(Articles), by = 1)
+barplot(table(Articles), beside = TRUE, ylim = c(0, 0.35*nrow(pub_data)), col = "#eff3ff" )
+lines(x+0.5,dpois(x,lambda=mean(Articles))*nrow(pub_data), col="#08519c",lwd = 2)
+title(xlab="Number of articles", ylab="Frequency")
+#why is this not 'beside' ? how to do this in ggplot?
 
-boxplot_fn <- function(x_data, x, xlab) 
-{
-  col <- c("#eff3ff","#c6dbef","#9ecae1","#3182bd","#08519c")
-  ggplot(pub_data, aes(x = as.factor(pub_data[,x]), y = articles)) +
-    geom_boxplot(show.legend = FALSE, fill = col[x]) + 
-    labs(y = "Articles published", x = xlab) +
-    scale_x_discrete()+
-    theme(
-      axis.title.x = element_text(size = 7.5),
-      axis.title.y = element_text(size = 7.5)
-    )
-}
-
-ggarrange(boxplot_fn(pub_data,2,"female"),
-          boxplot_fn(swim_data,3,"married") ,
-          boxplot_fn(swim_data,4,"kids")
-)
-
-
+#Model1
 pub.glm <- glm(articles ~ .,data = pub_data, family = poisson)
 summary(pub.glm)
-hist(pub_data$articles)
-x <- 0:20
-curve(dpois(lambda = mean(articles)))
-mean(articles)
+)
 
-pub_data[,1] %>% 
-  as_tibble() %>% 
-  ggplot(aes(value)) + 
-  geom_histogram(stat = "density") +
-  stat_function(fun = function(x) {dpois(x, mean(articles)), color = "red") 
-par(mfrow = c(2,1))
-barplot(dpois(0:20,mean(articles)),names = 0:20)
-hist(pub_data$articles,  breaks = 20, freq=FALSE)
-curve(dpois(x,lambda=mean(articles)), add=TRUE,col="red")
+#Archive
+  
+  ggplot(pub_data,aes (x= Mentor_publications, y = Articles))+
+    geom_point(position = position_jitter(width = 1))+
+    theme(legend.position = "top",
+          legend.direction = "horizontal",
+          plot.title = element_text(hjust = 0.5))+
+    labs(title = "Data distribution by category levels", x = "Mentor publications", y = "Articles published" )+
+    facet_grid(~Female)
+  
+  
+  boxplot_fn <- function(x_data, x, xlab) 
+  {
+    col <- c("#eff3ff","#c6dbef","#9ecae1","#3182bd","#08519c")
+    ggplot(pub_data, aes(x = as.factor(pub_data[,x]), y = Articles)) +
+      geom_boxplot(show.legend = FALSE, fill = col[x]) + 
+      labs(y = "Articles published", x = xlab) +
+      scale_x_discrete()+
+      theme(
+        axis.title.x = element_text(size = 7.5),
+        axis.title.y = element_text(size = 7.5)
+      )
+  }
+  
+  ggarrange(boxplot_fn(pub_data,2,"female"),
+            boxplot_fn(swim_data,3,"married") ,
+            boxplot_fn(swim_data,4,"kids")
+  )
