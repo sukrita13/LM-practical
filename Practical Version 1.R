b11f9a303a7553d55a5e3497909b1f0999fae730
sukrita13 <sukritac@gmail.com>
2021-12-02 13:48:40 +0000

    Version 1.2

--- Practical Version 1.R
+++ Practical Version 1.R
@@ -4,6 +4,8 @@ library(tidyverse)
 library(ggplot2)
 library(magrittr)
 library(corrgram)
+library(ggpubr)
+library(gridExtra)
 
 #Data load and summary 
 pub_data <- as.data.frame(readr::read_csv("pub.csv",
@@ -16,7 +18,12 @@ levels(pub_data$Marital_Status) <- c("Married","Unmarried")
 pub_data$Marital_Status <- fct_relevel(pub_data$Marital_Status, c("Unmarried","Married"))
 str(pub_data)
 
-pub_data %>% mutate_at("No_of_kids",as.factor)%>%summary
+pub_data_factored <- pub_data %>% 
+  mutate_at("No_of_kids",as.factor)%>%
+  mutate( Prestige_Cat= as.factor(paste(ceiling(Prestige_Score)-1,"-",ceiling(Prestige_Score)," ",sep = "")) )%>%
+  select(-Prestige_Score)
+
+summary(pub_data_factored)
 
 #Overdispersion summary
 c(Mean = mean(pub_data[,1]) ,
@@ -55,26 +62,67 @@ lines(x+0.5,dpois(x,lambda=mean(pub_data$Articles))*nrow(pub_data), col="#08519c
 title(xlab="Number of articles", ylab="Frequency")
 #why is this not 'beside' ? how to do this in ggplot?
 
+#Visualization for categorical data
+plot_by_category <- function(x_data,x, cat)  {
+  x_data %>%
+    ggplot( aes(x=x_data[,x], color="x_data[,cat]",alpha=.25, fill=x_data[,cat])) +
+    geom_histogram(aes(y=..density..), binwidth = 1, 
+                   position="identity")+
+    geom_density(alpha=.1) +
+    theme(
+      legend.position="none",
+      panel.spacing = unit(0.1, "lines"),
+      strip.text.x = element_text(size = 8)
+    ) +
+    #scale_color_brewer("Blues")+
+    #scale_fill_brewer("Blues")+
+    xlab(paste("By",colnames(x_data)[cat])) +
+    ylab("")+
+    facet_wrap(~x_data[,cat])
+}
+
+
+summary()
+
+data_trans <- pub_data %>% 
+              mutate(Prestige_Cat = ifelse(ceiling(Prestige_Score)==1,2,ceiling(Prestige_Score))) %>% 
+              mutate_at("Prestige_Cat", as.factor)
+A <- ifelse(
+            ceiling(pub_data[,5])==1,
+            2,
+            ceiling(pub_data[,5])
+            )
+summary(as.factor(A))
+plot_by_category(data_trans,1,7)
+
+
+grid.arrange(
+  arrangeGrob(plot_by_category(pub_data_factored,1,2), 
+              plot_by_category(pub_data_factored,1,3), 
+              nrow=2), 
+  plot_by_category(pub_data_factored,1,4) ,
+  nrow = 1,
+  top = "Distribution of number of articles published split by category levels")
 
 #Model1
 pub.glm <- glm(pub_data$Articles ~ .,data = pub_data, family = poisson)
 summary(pub.glm)
 
 #Archive
-  
-  ggplot(pub_data,aes (x= Prestige_Score, y = Articles, col = No_of_kids))+
-    geom_point(position = position_jitter(width = 1))+
+pub_data %>% group_by(Articles, Gender, Mentor_publications, Marital_Status) %>% summarise(value = length(Articles)) %>% 
+  ggplot(aes (x= Mentor_publications, y = Articles, size = value, col = Marital_Status))+
+    geom_point(alpha = 0.5)+
     theme(legend.position = "top",
           legend.direction = "horizontal",
           plot.title = element_text(hjust = 0.5))+
     labs(title = "Data distribution by category levels", x = "Mentor publications", y = "Articles published" )+
-    facet_grid(~Female)
+    facet_grid(~Gender)
   
   
   boxplot_fn <- function(x_data, x, xlab) 
   {
     col <- c("#eff3ff","#c6dbef","#9ecae1","#3182bd","#08519c")
-    ggplot(pub_data, aes(x = as.factor(pub_data[,x]), y = Articles)) +
+    ggplot(pub_data, aes(x = as.factor(pub_data[,x]), y = (Articles))) +
       geom_boxplot(show.legend = FALSE, fill = col[x]) + 
       labs(y = "Articles published", x = xlab) +
       scale_x_discrete()+
@@ -89,4 +137,10 @@ summary(pub.glm)
             boxplot_fn(swim_data,4,"kids")
   )
   
+  
+  
 detach(pub_data)
+
+  
+
+
